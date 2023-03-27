e8254107171ca965c45adbdb58265ac01e5e2d65
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-27 12:07:09 +0600

    route settep

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CanceledPage from "./pages/CanceledPage";
+import CompletedPage from "./pages/CompletedPage";
+import CreatePage from "./pages/CreatePage";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+import NewTaskPage from "./pages/NewTaskPage";
+import Page404 from "./pages/Page404";
+import ProfilePage from "./pages/ProfilePage";
+import ProgressPage from "./pages/ProgressPage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1>Task Manager</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/all" element={<NewTaskPage />} />
+          <Route path="/progress" element={<ProgressPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/completed" element={<CompletedPage />} />
+          <Route path="/canceled" element={<CanceledPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
