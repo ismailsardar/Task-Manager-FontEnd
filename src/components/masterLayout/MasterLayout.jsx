ba67678aad6d22f86330637032fb7e512e8dd4fa
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-28 15:00:55 +0600

    master Layout finish

--- src/components/masterLayout/MasterLayout.jsx
+++ src/components/masterLayout/MasterLayout.jsx
@@ -14,14 +14,31 @@ import user from "../../assets/img/ismile.png";
 import logo from "../../assets/img/logo.svg";
 
 const MasterLayout = (props) => {
-  let contentRef,
-    sideNavRef = useRef();
+  let contentRef,sideNavRef = useRef();
+
+  const MenuBarClickHandler = () => {
+    let sideNav = sideNavRef;
+    let content = contentRef;
+
+    if (sideNav.classList.contains("side-nav-open")) {
+      sideNav.classList.add("side-nav-close");
+      sideNav.classList.remove("side-nav-open");
+      content.classList.add("content-expand");
+      content.classList.remove("content");
+    } else {
+      sideNav.classList.remove("side-nav-close");
+      sideNav.classList.add("side-nav-open");
+      content.classList.remove("content-expand");
+      content.classList.add("content");
+    }
+  };
+
   return (
     <>
       <Navbar className="fixed-top px-0 shadow-sm">
         <Container fluid={true}>
           <Navbar.Brand>
-            <a className="icon-nav m-0 h5">
+            <a className="icon-nav m-0 h5" onClick={MenuBarClickHandler}>
               <AiOutlineMenuUnfold />
             </a>
             <img className="nav-logo mx-2" src={logo} alt="logo" />
