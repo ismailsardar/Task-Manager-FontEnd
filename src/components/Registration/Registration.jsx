5a42a9d166c1dce1a6c23b30251764b954b857e5
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-29 10:38:45 +0600

    on Registration function add Registration component

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React,{useRef} from "react";
 import { Link } from "react-router-dom";
 
 const Registration = () => {
+  let emailRef,
+    firstNameRef,
+    lastNameRef,
+    mobileRef,
+    passwordRef = useRef();
+
+  const onRegistration = () => {
+    let email = emailRef.value;
+    let firstName = firstNameRef.value;
+    let lastName = lastNameRef.value;
+    let mobile = mobileRef.value;
+    let password = passwordRef.value;
+    let photo = "demo";
+
+    debugger;
+    console.log(email + firstName + lastName + mobile + password);
+  };
+
   return (
     <>
       <div className="container">
@@ -16,6 +34,7 @@ const Registration = () => {
                     <div className="col-md-4 p-2">
                       <label>Email Address</label>
                       <input
+                        ref={(input) => (emailRef = input)}
                         type="email"
                         placeholder="Email"
                         className="form-control animated fadeInUp"
@@ -24,6 +43,7 @@ const Registration = () => {
                     <div className="col-md-4 p-2">
                       <label>First Name</label>
                       <input
+                        ref={(input) => (firstNameRef = input)}
                         type="text"
                         placeholder="First Name"
                         className="form-control animated fadeInUp"
@@ -32,6 +52,7 @@ const Registration = () => {
                     <div className="col-md-4 p-2">
                       <label>Last Name</label>
                       <input
+                        ref={(input) => (lastNameRef = input)}
                         type="text"
                         placeholder="Last Name"
                         className="form-control animated fadeInUp"
@@ -40,6 +61,7 @@ const Registration = () => {
                     <div className="col-md-4 p-2">
                       <label>Mobile Number</label>
                       <input
+                        ref={(input) => (mobileRef = input)}
                         type="mobile"
                         placeholder="Mobile Number"
                         className="form-control animated fadeInUp"
@@ -48,6 +70,7 @@ const Registration = () => {
                     <div className="col-md-4 p-2">
                       <label>Password</label>
                       <input
+                        ref={(input) => (passwordRef = input)}
                         type="password"
                         placeholder="Password"
                         className="form-control animated fadeInUp"
@@ -56,7 +79,10 @@ const Registration = () => {
                   </div>
                   <div className="row mt-2 p-0">
                     <div className="col-md-4 p-2">
-                      <button className="btn btn-primary mt-3 w-100 float-end animated fadeInUp">
+                      <button
+                        onClick={onRegistration}
+                        className="btn btn-primary mt-3 w-100 float-end animated fadeInUp"
+                      >
                         Complete
                       </button>
                     </div>
