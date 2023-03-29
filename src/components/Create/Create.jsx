8508e5a4c58c475d5325236b3d3f17f9ac2180ba
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-29 09:50:00 +0600

    create task component

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
 
 const Create = () => {
   return (
-    <div>Create</div>
-  )
-}
+    <>
+      <Container fluid={true} className="content-body">
+        <Row className="d-flex justify-content-center">
+          <div className="col-12 col-sm-12 col-lg-8 col-md-8 p-2">
+            <div className="card">
+              <div className="card-body">
+                <h4>Create New</h4>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Task Name"
+                  className="form-control animated fadeInUp"
+                />
+                <br />
+                <textarea
+                  placeholder="Task Descriptions..."
+                  className="form-control animated fadeInUp"
+                />
+                <br />
+                <button className="btn btn-primary float-end">Create</button>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-export default Create
+export default Create;
