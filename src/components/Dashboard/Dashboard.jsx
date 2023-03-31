d938e36b36703297e9d7b837ac3a019471586c6f
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-31 16:32:50 +0600

    dashboard created

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { SummaryRequest } from "../../APIRequest/ApiRequest";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  useEffect(() => {
+    SummaryRequest();
+  }, []);
+
+  let summaryList = useSelector((state) => state.summary.value);
+
   return (
     <>
       <div className="container">
         <div className="row p-0 m-0">
-          <div className="col-12 col-lg-3 col-md-3 col-sm-6 p-2">
-            <div className="card h-100">
-              <div className="card-body">
-                <h5 className="animated fadeInUp">Total</h5>
-                <h6 className="text-secondary animated fadeInUp">00</h6>
+          {summaryList.map((item, i) => (
+            <div key={i.toString()} className="col-12 col-lg-3 col-md-3 col-sm-6 p-2">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="animated fadeInUp">{item._id}</h5>
+                  <h6 className="text-secondary animated fadeInUp">
+                    Total - {item.sum}
+                  </h6>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
