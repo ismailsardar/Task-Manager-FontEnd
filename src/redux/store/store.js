a51d551242f418bd34afce341d666cf6b09db639
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-31 16:21:24 +0600

    summary Reducer add

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import settingsReducer from "../slice/settingsSlice";
+import summaryReducer from "../slice/summarySlice";
 import taskReducer from "../slice/taskSlice";
 
 export default configureStore({
     reducer:{
         settings: settingsReducer,
         task: taskReducer,
+        summary: summaryReducer,
     }
 });
